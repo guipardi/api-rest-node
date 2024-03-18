795eac027c1ce6e177f53cf6844915e09415ac52
Guilherme Pardi <guilhermepardi@gmail.com>
2024-03-18 12:44:38 -0300

    test fix

--- db/migrations/20240304130830_add-session-id-to-transactions.ts
+++ db/migrations/20240304130830_add-session-id-to-transactions.ts
@@ -2,7 +2,7 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transactions', (table) => {
-    table.decimal('session_id').after('id').index()
+    table.string('session_id').after('id').index()
   })
 }
 
